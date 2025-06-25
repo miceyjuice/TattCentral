de0699ec6ecb747af8ba4a829f58a5cd89d4eaef
Paweł Szumiec <pawel.szumiec98@gmail.com>
2025-06-25 17:13:10 +0200

    feat: Add RecentWorkSection to LandingPage with image gallery

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -2,8 +2,9 @@ const LandingPage = () => {
 	return (
 		<>
 			<Navigation />
-			<main className="flex flex-col items-center justify-center max-w-7xl mx-auto w-full mt-20">
+			<main className="flex flex-col gap-8 items-center justify-center max-w-7xl mx-auto w-full my-10">
 				<HeroSection />
+				<RecentWorkSection />
 			</main>
 		</>
 	);
@@ -45,6 +46,36 @@ const HeroSection = () => {
 				<h1 className="z-20 text-4xl font-bold mb-4">Book your next masterpiece</h1>
 				<p className="z-20 text-lg">Your one-stop destination for all things tattoo.</p>
 				<button className="z-20 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Book now</button>
+			</div>
+		</section>
+	);
+};
+
+const RecentWorkSection = () => {
+	return (
+		<section className="flex flex-col gap-4 w-full px-10 mt-10">
+			<div className="flex items-center justify-between">
+				<h2 className="text-2xl font-bold">Recent work</h2>
+				<a href="/gallery" className="hover:underline">
+					View all
+				</a>
+			</div>
+			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6">
+				<div className=" rounded-lg">
+					<img src="src/assets/images/tattoo-example-1.png" alt="Tattoo 1" className="w-full h-auto rounded-lg" />
+				</div>
+				<div className="rounded-lg">
+					<img src="src/assets/images/tattoo-example-2.png" alt="Tattoo 2" className="w-full h-auto rounded-lg" />
+				</div>
+				<div className="rounded-lg">
+					<img src="src/assets/images/tattoo-example-3.png" alt="Tattoo 3" className="w-full h-auto rounded-lg" />
+				</div>
+				<div className="rounded-lg">
+					<img src="src/assets/images/tattoo-example-4.png" alt="Tattoo 4" className="w-full h-auto rounded-lg" />
+				</div>
+				<div className="rounded-lg">
+					<img src="src/assets/images/tattoo-example-5.png" alt="Tattoo 5" className="w-full h-auto rounded-lg" />
+				</div>
 			</div>
 		</section>
 	);
