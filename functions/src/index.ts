29fe03dc87376386867cae7154fb0c72a71b134a
Paweł Szumiec <pawel.szumiec98@gmail.com>
2025-12-07 20:57:28 +0100

    chore: update Firebase initialization and fix export paths

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,9 +2,14 @@
  * Firebase Cloud Functions for TattCentral
  *
  * Email notification functions triggered by Firestore document changes
+ * @version 2.0.0 - Added cancellation token generation and cancel link in emails
  */
 
+import { initializeApp } from "firebase-admin/app";
 import { setGlobalOptions } from "firebase-functions/v2";
+
+// Initialize Firebase Admin SDK
+initializeApp();
 
 // Set global options for all functions
 setGlobalOptions({
@@ -13,5 +18,5 @@ setGlobalOptions({
 });
 
 // Export appointment triggers
-export { onAppointmentCreated } from "./triggers/on-appointment-created";
-export { onAppointmentUpdated } from "./triggers/on-appointment-updated";
+export { onAppointmentCreated } from "./triggers/on-appointment-created.js";
+export { onAppointmentUpdated } from "./triggers/on-appointment-updated.js";
